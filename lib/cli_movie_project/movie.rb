9b51fcc55e34a5a075cbbe53792c8aca7110ca79
Beekasha <brian.j.kleiner@gmail.com>
2020-02-12 12:34:59 -0500

    basic functionality added, w api calls working

--- lib/cli_movie_project/movie.rb
+++ lib/cli_movie_project/movie.rb
@@ -1,10 +1,29 @@
 class MovieDictionary::Movie
-    attr_accessor :title, :actors, :plot, :director, :awards, :rating, :year
+    attr_accessor :Title, :Actors, :Plot, :Director, :Awards, :BoxOffice, :Year
+
+    @@all = []
+
+    def initialize(hash)
+        hash.each_pair do |k, v|
+            instance_variable_set("@#{k}", v)
+        end
+        self.display_movie
+    end
+
+    def display_movie
+        puts "#{self.Title} is a #{self.Year} film directed by #{self.Director}."
+        puts "Would you like to know more?(y/n)"
+        input = gets.strip
+        if input == "y"
+            puts "more info"
+        end
+            
+    end
 
     
     
     def self.all
-        #returns all searched movies
+        @@all
  
 
     end
