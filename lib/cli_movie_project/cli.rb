85936e82374f18114f2406425718eb5e2579223b
Beekasha <brian.j.kleiner@gmail.com>
2020-02-11 23:05:46 -0500

    fleshing out the cli

--- lib/cli_movie_project/cli.rb
+++ lib/cli_movie_project/cli.rb
@@ -3,5 +3,30 @@
 class MovieDictionary::CLI
     def call
         puts "Welcome to the Movie Dictionary!"
+        main_menu
     end
+
+    def main_menu
+        puts "What movie would you like to like to learn more about?"
+        get_movie_info
+    end
+
+    def movie_menu
+        puts "Would you like to know more about this movie?"
+        puts "1. Actors"
+        puts "2. Plot"
+        puts "3. Awards"
+    end
+
+    def watchlist
+        puts "return the watchlist here"
+    end
+
+    def get_movie_info
+        input = gets.strip
+        # while input != "exit
+        puts "puts out the movie info from api"
+    end
+
+
 end
