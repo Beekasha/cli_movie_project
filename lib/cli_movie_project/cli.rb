554b3fe21387e49ff7f73f89a9cfeae4aa7cfe08
Beekasha <brian.j.kleiner@gmail.com>
2020-02-12 21:45:41 -0500

    basically working, needs watchlist functionality

--- lib/cli_movie_project/cli.rb
+++ lib/cli_movie_project/cli.rb
@@ -3,34 +3,65 @@ require "pry"
 require 'dotenv/load'
 
 class MovieDictionary::CLI
+
+    @@current_movie = []
+
     def call
+        puts
         puts "Welcome to the Movie Dictionary!"
         main_menu
     end
 
     def main_menu
+        puts
         puts "What movie would you like to like to learn more about?"
-        get_movie_info
+        puts 'Or, you can view your watchlist by typing "watchlist".'
+        puts
+        input = gets.strip
+        if input == "watchlist"
+            MovieDictionary::Movie.display_watchlist
+            main_menu
+        elsif input != "exit"
+            converted_title = convert_movie_title(input)
+            new_movie = create_movie_from_api(converted_title)
+            @@current_movie = new_movie
+            new_movie.display_movie
+            movie_menu
+            
+        end
+
+
+
     end
 
     def movie_menu
-        puts "Would you like to know more about this movie?"
-        puts "1. Actors"
-        puts "2. Plot"
-        puts "3. Awards"
+        puts
+        puts "Would you like to know more about this movie? (y/n)"
+        input = gets.chomp
+        if input == "y"
+            puts @@current_movie.display_more_info
+            puts "Do you want to add #{@@current_movie.Title} (#{@@current_movie.Year}) to your watchlist?"
+            input_2 = gets.chomp
+            if input_2 == "y"
+                @@current_movie.add_to_watchlist
+
+            end
+            
+        else
+
+            main_menu
+        end
+        main_menu
     end
 
     def watchlist
         puts "return the watchlist here"
     end
 
-    def get_movie_info
-        input = gets.strip
-        # while input != "exit
-        api_input = input.split(" ").join("+")
+    def convert_movie_title(user_input_title) #returns input for API
         
-        create_movie_from_api(api_input)
-
+        user_input_title.split(" ").join("+")
+        
     end
 
     def create_movie_from_api(title)
@@ -42,7 +73,7 @@ class MovieDictionary::CLI
         hash = JSON.parse(response, symbolize_names: true)
         new_movie = MovieDictionary::Movie.new(hash)
         # example_hash = {:Title=>"Iron Man", :Year=>"2008", :Rated=>"PG-13", :Released=>"02 May 2008", :Runtime=>"126 min", :Genre=>"Action, Adventure, Sci-Fi", :Director=>"Jon Favreau", :Writer=>"Mark Fergus (screenplay), Hawk Ostby (screenplay), Art Marcum (screenplay), Matt Holloway (screenplay), Stan Lee (characters), Don Heck (characters), Larry Lieber (characters), Jack Kirby (characters)", :Actors=>"Robert Downey Jr., Terrence Howard, Jeff Bridges, Gwyneth Paltrow", :Plot=>"After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.", :Language=>"Hungarian, Kurdish, Hindi, English, Persian, Urdu, Arabic", :Country=>"USA", :Awards=>"Nominated for 2 Oscars. Another 21 wins & 65 nominations.", :Poster=>"https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg", :Ratings=>[{:Source=>"Internet Movie Database", :Value=>"7.9/10"}, {:Source=>"Rotten Tomatoes", :Value=>"94%"}, {:Source=>"Metacritic", :Value=>"79/100"}], :Metascore=>"79", :imdbRating=>"7.9", :imdbVotes=>"896,884", :imdbID=>"tt0371746", :Type=>"movie", :DVD=>"30 Sep 2008", :BoxOffice=>"$318,298,180", :Production=>"Paramount Pictures", :Website=>"N/A", :Response=>"True"}
-        main_menu
+        new_movie
         
     end
 
--- lib/cli_movie_project/movie.rb
+++ lib/cli_movie_project/movie.rb
@@ -1,36 +1,108 @@
 class MovieDictionary::Movie
-    attr_accessor :Title, :Actors, :Plot, :Director, :Awards, :BoxOffice, :Year
+    attr_accessor :Title, :Actors, :Plot, :Director, :Awards, :Year, :Writer
 
     @@all = []
+    @@watchlist = []
 
     def initialize(hash)
         hash.each_pair do |k, v|
             instance_variable_set("@#{k}", v)
         end
-        self.display_movie
+        @@all << self
+        # self.display_movie
     end
 
     def display_movie
+        puts
         puts "#{self.Title} is a #{self.Year} film directed by #{self.Director}."
-        puts "Would you like to know more?(y/n)"
-        input = gets.strip
-        if input == "y"
-            puts "more info"
-        end
+        puts
+        # puts "Would you like to know more?(y/n)"
+        # input = gets.strip
+        # if input == "y"
+        #     puts "Actors: #{self.Actors}"
+        #     puts "Plot: #{self.Plot}"
+        #     if self.Awards != []
+        #         puts "Awards: #{self.Awards}"
+        #     end
+        # end
+        # puts "Would you like to add #{self.Title} (#{self.Year}) to your watchlist? (y/n)"
+        # input = gets.strip
+        # if input == y
+        #     @@watchlist << self
+        # else
             
+        # end
+
+            #should go back to main menu
+    end
+
+    def display_more_info
+        puts
+        puts "Actors: #{self.Actors}"
+        puts
+        puts "Plot: #{self.Plot}"
+        puts
+        if self.Awards != []
+            puts "Awards: #{self.Awards}"
+            puts
+        end
+        puts "Written By: #{self.Writer}"
+        puts
+    end
+
+    def self.display_watchlist
+        if @@watchlist == []
+            puts
+            puts "Sorry, your watchlist is empty."
+            puts
+        else
+            @@watchlist.each.with_index(1) do |movie, i|
+                puts
+                puts "#{i}. #{movie.Title} (#{movie.Year})"
+            end
+        end
     end
 
     
     
     def self.all
         @@all
- 
-
     end
 
-    # film = self.new
-    # film.title = "American Beauty"
-    # film.director = "Sam Mendez"
+    # def add_to_watchlist
+    #     if @@watchlist.include?(self)
+    #         puts "#{self.Title} is already on your watchlist."
+    #     else
+    #         @@watchlist << self
+    #         puts "#{self.Title} was successfully added to your watchlist."
+    #         puts
+    #     end
+    # end
+
+
+    def add_to_watchlist
+        if @@watchlist == []
+            @@watchlist << self
+            puts "#{self.Title} was successfully added to your watchlist."
+            puts
+        else
+            @@watchlist.each do |movie|
+                if movie.Title == self.Title
+                    puts
+                    puts "#{self.Title} is already on your watchlist."
+                else
+                    @@watchlist << self
+                    puts "#{self.Title} was successfully added to your watchlist."
+                    puts
+                
+                end
+            end
+        end
+    end
+
+    
+
+  
 
     
 end
